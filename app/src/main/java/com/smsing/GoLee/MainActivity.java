3dce1a2179ef54c65c3cbc6cdda9efcbaa482b94
LEEjaeHyun <swaa23@gmail.com>
2020-05-17 21:43:23 +0900

    바텀네비게이션 비치 , 프래그먼트생명주기 , 상단타이틀바 제거

--- app/src/main/java/com/smsing/GoLee/MainActivity.java
+++ app/src/main/java/com/smsing/GoLee/MainActivity.java
@@ -5,9 +5,14 @@ import android.app.DownloadManager;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -41,8 +46,15 @@ public class MainActivity extends AppCompatActivity {
     // 서버 주소값 및 url 포멧은 항상 고정적  .
     final static String serverdomain ="http://13.125.164.239/post";
     final static String url = "url";
-    // Oncreate
 
+    // FrameLayout에 각 메뉴의 Fragment를 바꿔 줌
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private SubFragment subfragment = new SubFragment();
+    private HomeFragment homefragment = new HomeFragment();
+    private SettingFragment settingfragment = new SettingFragment();
+
+
+    // Oncreate Activity 생명주기
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,11 +62,46 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SplashActivity.class);
         restfulRequest();
         startActivity(intent);
+        BottomNavigationOperate();
+
     }
+    private void BottomNavigationOperate(){
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        // 첫 화면 지정
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, homefragment).commitAllowingStateLoss();
+        // bottomNavigationView의 아이템이 선택될 때 호출될 리스너 등록
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        //TODO 처음 실행했을때 화면 종류에 관계없이 바텀네비게이션이 첫번째 인덱스에 고정되어있음
+        //TODO 수정필요
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()) {
+                    case R.id.action_sub: {
+                        transaction.replace(R.id.frame_layout, subfragment).commitAllowingStateLoss();
+                        Toast.makeText(getApplicationContext(),"menu1",Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    case R.id.action_home: {
+                        transaction.replace(R.id.frame_layout, homefragment).commitAllowingStateLoss();
+                        Toast.makeText(getApplicationContext(),"menu2",Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    case R.id.action_setting: {
+                        transaction.replace(R.id.frame_layout, settingfragment).commitAllowingStateLoss();
+                        Toast.makeText(getApplicationContext(),"menu3",Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                }
+                return true;
+
+            }
+        });
+    }
+
     //RestfulRequest ( get 요청 함 )
-
     public void restfulRequest(){
-
         //RequestQueue  서버 요청자. 다른 Request 클래스들의 정보대로 서버에 요청을 보내는 역할
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         // Json 형태로 Post를 요청하기 위해서 JSONObject를 사용함
@@ -82,7 +129,6 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("error","Error getting response");
                     }
                 });
-
         //RequestQueue 객체의 add( ) 함수에 Request 객체를 매개변수로 지정하여 호출하면 서버 연동이 발생합니다.
         requestQueue.add(jsonObjectRequest);
     }
