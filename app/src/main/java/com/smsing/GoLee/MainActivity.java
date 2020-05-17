86e6e0f5859441bd7db08c9e5a5b3a5f94bb5b3d
LEEjaeHyun <swaa23@gmail.com>
2020-05-17 18:05:08 +0900

    Json format Post방식 추가

--- app/src/main/java/com/smsing/GoLee/MainActivity.java
+++ app/src/main/java/com/smsing/GoLee/MainActivity.java
@@ -22,6 +22,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
@@ -35,43 +36,55 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    //TODO 서버 주소 및 URL 경로는 유동적으로 변경해야함
-    String url ="http://13.125.164.239/get/http://malware.testing.google.test/testing/malware/";
 
+    // 변수 선언 부
+    // 서버 주소값 및 url 포멧은 항상 고정적  .
+    final static String serverdomain ="http://13.125.164.239/post";
+    final static String url = "url";
     // Oncreate
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, com.smsing.GoLee.SplashActivity.class);
-        makeRequest();
+        Intent intent = new Intent(this, SplashActivity.class);
+        restfulRequest();
         startActivity(intent);
     }
+    //RestfulRequest ( get 요청 함 )
 
-    //makeRequest ( get 요청 함 )
-    public void makeRequest(){
+    public void restfulRequest(){
+
         //RequestQueue  서버 요청자. 다른 Request 클래스들의 정보대로 서버에 요청을 보내는 역할
-        final RequestQueue queue = Volley.newRequestQueue(this);
-        Log.d("Log point1 ","log point1"+queue.toString());
-        //StringRequest 문자열을 결과로 받는 요청
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                //응답 리스너 설정
-                new Response.Listener<String>() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        // Json 형태로 Post를 요청하기 위해서 JSONObject를 사용함
+        JSONObject object = new JSONObject();
+        try {
+            //TODO value 값은 메신저에서 하드코딩 방식이 아닌 메시지에서 받은
+            // 변수값을 넣어주는 작업을 해야함
+            object.put(url,"www.naver.com");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("Object Log Point", object.toString());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, serverdomain, object,
+                new Response.Listener<JSONObject>() {
+                    // jsonObjectRequest 정상적으로 실행했을 경우 반환값
                     @Override
-                    public void onResponse(String response) {
-                        //응답 결과처리
-                        Log.d("Response is: ", response);
-                    };
+                    public void onResponse(JSONObject response) {
+                        Log.d("OnResponse","String Response : "+ response.toString());
+                    }
                 },
+                //응답실패시
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        //응답 실패시
-                        Log.d("That didn't work!",error.toString());
+                        Log.d("error","Error getting response");
                     }
                 });
+
         //RequestQueue 객체의 add( ) 함수에 Request 객체를 매개변수로 지정하여 호출하면 서버 연동이 발생합니다.
-        queue.add(stringRequest);
+        requestQueue.add(jsonObjectRequest);
     }
 
 }
